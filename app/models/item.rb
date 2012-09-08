0f0a865c6ed6091083598a98cc812ec507172ce3
Akiko Saito <akiko74@yahoo.co.jp>
2012-09-08 16:46:44 +0900

    Modified models for making relationship

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ActiveRecord::Base
   attr_accessible :name
+  has_many :ingredients
   has_many :recipes, :through => :ingredients
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
   attr_accessible :name
+  has_many :recipes
 end
