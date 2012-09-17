4077bde3586b1430a632075320dd65cd889d71e9
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2012-09-17 17:44:29 +0900

    未認証ユーザーからのアクセスにかんしては/admin/sign_inではなく404を返すように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 #  admin権限登録後に以下を有効にする。
-  before_filter :authenticate_admin!, :except => [ :index, :show]
+  before_filter :authenticate_admin, :except => [ :index, :show]
+
+  def authenticate_admin
+    unless admin_signed_in?
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
 end
