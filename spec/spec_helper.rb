4628b3791784fe7ef37e61fbfd06dd4c081367fe
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2012-09-17 16:04:00 +0900

    rspec周りを設定

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'factory_girl'
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.use_transactional_fixtures = true
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+  end
+
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
---
+++ spec/support/dqx_items.rb
@@ -0,0 +1,70 @@
+share_examples_for 'the Request that is returned "HTTP OK"' do
+  it { should be_success }
+  its(:status) { should equal 200 }
+end
+
+share_examples_for 'the Request that is returned "HTTP Found"' do
+  it { should be_redirect }
+  its(:status) { should equal 302 }
+end
+
+share_examples_for 'the Request that is redirected to "Home Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP Found"'
+  it { should redirect_to root_path }
+end
+
+share_examples_for 'the Request that is returned "Item list Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'items/index' }
+end
+
+share_examples_for 'the Request that is returned "Recipe list Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'recipes/index' }
+end
+
+share_examples_for 'the Request that is returned "Job list Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'jobs/index' }
+end
+
+share_examples_for 'the Request that is returned "Item regist Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'items/new' }
+end
+
+share_examples_for 'the Request that is returned "Recipe regist Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'recipes/new' }
+end
+
+share_examples_for 'the Request that is returned "Job regist Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'jobs/new' }
+end
+
+share_examples_for 'the Request that is returned "Item edit Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'items/edit' }
+end
+
+share_examples_for 'the Request that is returned "Recipe edit Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'recipes/edit' }
+end
+
+share_examples_for 'the Request that is returned "Job edit Page"' do
+  it_should_behave_like 'the Request that is returned "HTTP OK"'
+  it { should render_template "layouts/application" }
+  it { should render_template 'jobs/edit' }
+end
+
+
