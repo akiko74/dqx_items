429b3357f33159dfdcdde4060023c5b7d786e62f
Akiko Saito <akiko74@yahoo.co.jp>
2012-09-19 12:42:14 +0900

    登録テスト用に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 #  admin権限登録後に以下を有効にする。
-  before_filter :authenticate_admin!, :except => [ :index, :show]
+#  before_filter :authenticate_admin, :except => [ :index, :show]
 end
