c98dfc41315ad297f7fe142c0ec9877ac1af6513
Akiko Saito <akiko74@yahoo.co.jp>
2013-01-12 16:28:48 +0900

    Itemに金額を入れるフィールドを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910111657) do
+ActiveRecord::Schema.define(:version => 20130112072548) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20120910111657) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "price"
   end
 
   create_table "jobs", :force => true do |t|
