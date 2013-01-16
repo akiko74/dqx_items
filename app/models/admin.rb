92af539e7098fa9f9f53e23b16399844f4e8b471
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-01-17 07:05:36 +0900

    Modiry controllers design.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,8 +2,9 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
+  devise :database_authenticatable, :rememberable, :timeoutable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
