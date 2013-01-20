3478390902153f1ecded2200de560c6793ffbdf5
Akiko Saito <akiko74@yahoo.co.jp>
2013-01-20 13:15:48 +0900

    merged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-
 # 基底コントローラ
 class ApplicationController < ActionController::Base
 end
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+
+  def add_ingredient_link(name)
+    link_to_function name do |page|
+      page.insert_html :bottom, :ingredients, :partial => 'ingredient', :object => Ingredient.new
+    end
+  end
+
 end
