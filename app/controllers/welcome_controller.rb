b4bca1271a4133cdbd153e429e5445e7528e5c81
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-03-11 03:11:41 +0900

    Add my/items link and updates my items form; but it's not enabled.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,6 +9,12 @@
 class WelcomeController < ApplicationController
 
   # トップページ
+  #
+  # @example
+  # +非ログインユーザー -> recipes#index
+  # +ログイン済みユーザー -> my/items#index
+  # +ログイン済み管理者 -> recipes#index
+  #
   def index
     if user_signed_in?
       return redirect_to my_items_path
