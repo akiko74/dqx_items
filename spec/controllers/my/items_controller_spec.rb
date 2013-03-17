d3709c2ad7faaa6166c97564cae7c9e50d1cbac3
Takahiro HAMAGUCHI <tk.hamaguchi@gmail.com>
2013-03-17 16:02:20 +0900

    Add My::ItemsController spec base.

--- spec/controllers/my/items_controller_spec.rb
+++ spec/controllers/my/items_controller_spec.rb
@@ -13,7 +13,7 @@ describe My::ItemsController do
   before {
     controller.stub!(:current_user).and_return(current_user)
     controller.stub!(:authenticate_user!).and_return(true)
-    #controller.stub!(:parse_character_items_stocks).and_return(true)
+    controller.stub!(:parse_equipments_items_json).and_return({})
   }
 
   context "#index" do
@@ -34,8 +34,8 @@ describe My::ItemsController do
         controller.should_receive(:authenticate_user!)
         subject
       end
-      it "should not call controller#parse_character_items_stocks" do
-        controller.should_not_receive(:parse_character_items_stocks)
+      it "should not call controller#parse_equipments_items_json" do
+        controller.should_not_receive(:parse_equipments_items_json)
         subject
       end
     end
@@ -44,7 +44,17 @@ describe My::ItemsController do
 
   context "#updates" do
     let(:required_params) { { :format => :json } }
-    let(:append_params) { {"character_items_stocks"=>{"0"=>{"character_id"=>"0", "item_id"=>"3", "stock"=>"4", "total_cost"=>"0"}}} }
+    let(:append_params) {
+      {
+        "equipments" => {
+          "0" => { "name" => "初級魔法戦士服", "stock" => "1", "renkin_count" => "0", "total_cost" => "1260" }
+        },
+        "items" => {
+          "0" => { "name" => "あやかしそう", "stock" => "-3" },
+          "1" => { "name" => "コットン草",   "stock" => "-3" }
+        }
+      } 
+    }
     let(:req) { put :updates, params }
     context "response" do
       before { req }
@@ -62,8 +72,8 @@ describe My::ItemsController do
         controller.should_receive(:authenticate_user!)
         subject
       end
-      it "should call controller#parse_character_items_stocks" do
-        controller.should_receive(:parse_character_items_stocks)
+      it "should call controller#parse_equipments_items_json" do
+        controller.should_receive(:parse_equipments_items_json)
         subject
       end
     end
@@ -71,7 +81,7 @@ describe My::ItemsController do
   end
 
   context "#parse_equipments_items_json" do
-    context "controller" do
+    context "controller's" do
 
       let(:expected_results) {
         {
@@ -98,7 +108,7 @@ describe My::ItemsController do
 
       before {
         controller.stub!(:params).and_return(request_params)
-      #  controller.unstub!(:parse_equipments_items_json)
+        controller.unstub!(:parse_equipments_items_json)
       }
 
       subject { controller.send(:parse_equipments_items_json) }
@@ -114,34 +124,6 @@ describe My::ItemsController do
         subject { controller.instance_variable_get(:@requested_equipments_items) }
         it { should == expected_results }
       end
-=begin
-    before {
-      controller.stub!(:params).and_return(request_params)
-      put :updates, :format => 'json'
-    }
-    context "response" do
-      subject { response }
-      it { should be_succes }
-    end
-    context "instance variables" do
-      subject { controller.instance_variables }
-      it { should include :@requested_equipments_items }
-    end
-
-    context "@requested_equipments_items" do
-      subject { controller.instance_variable_get(:@requested_equipments_items) }
-      it { should == expected_results }
-    end
-
-#    it { should be_success }
-#    it { should be_true }
-#    it { should be_instance_of Array }
-#    it { should == [] }
-#    it { controller.instance_variables.should == [] }
-#    it { assigns[:@character_items_stocks].should == "" }
-#    it { controller.instance_variable_get(:@character_items_stocks).should_not be_nil }
-
-=end
 
     end
 
